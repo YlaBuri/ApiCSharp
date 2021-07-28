d1b6b8766b876619529646bed5fd0e593b5b16f6
Yla Buri <ylaburi@gmail.com>
2021-07-28 16:12:04 -0300

    projeto inicial

---
+++ Calculadora/api/api/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PersonController : ControllerBase
+	{
+		private readonly ILogger<PersonController> _logger;
+
+		public PersonController(ILogger<PersonController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet("sum/{firstnumber}/{secondnumber}")]
+		public IActionResult Get(String firstnumber, String secondnumber)
+		{
+			if(IsNumeric(firstnumber) && IsNumeric(secondnumber))
+			{
+				var sum = ConvertToDecimal(firstnumber) + ConvertToDecimal(secondnumber);
+				return Ok(sum.ToString());
+			}
+			return BadRequest("Invalid input");
+		}
+
+		
+
+		private bool IsNumeric(string strNumber)
+		{
+			double number;
+			bool isNumber = double.TryParse(
+				strNumber, 
+				System.Globalization.NumberStyles.Any, 
+				System.Globalization.NumberFormatInfo.InvariantInfo, 
+				out number);
+			return isNumber;
+		}
+		private decimal ConvertToDecimal(string strNumber)
+		{
+			decimal decimalValue;
+			if(decimal.TryParse(strNumber, out decimalValue))
+			{
+				return decimalValue;
+			}
+			return 0;
+		}
+
+		
+	}
+}
---
+++ Calculadora/api/api/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model
+{
+	public class Person
+	{
+		public int Id { get; set; }
+
+		public String FirstName { get; set; }
+
+		public String Address { get; set; }
+
+		public String Gender { get; set; }
+	}
+}
